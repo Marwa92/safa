736adaf1773e6260c8f82be9b92e8808d2bdef80
Marwa92 <engmarwayehya@yahoo.com>
2020-10-20 09:16:00 +0200

    create shared components

---
+++ src/components/CountrySelector/index.js
@@ -0,0 +1,127 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import styled from "styled-components";
+import arrow from "../../assets/keyboard_arrow_down.svg";
+import PropTypes from "prop-types";
+
+const Container = styled.div`
+  width: 100%;
+  padding-left: 0;
+`;
+const Select = styled(Form.Control)`
+  height: ${(props) => props.height};
+  width: ${(props) => props.width};
+  appearance: none;
+  background-repeat: no-repeat, repeat;
+  background-position: right 1.1em top 50%, 0 0;
+  background-size: ${(props) => props.backgroundSize};
+  border-radius: ${(props) => props.borderRadius};
+  border: ${(props) => props.border};
+  background-color: ${(props) => props.backgroundColor};
+  box-sizing: ${(props) => props.boxSizing};
+  box-shadow: ${(props) => props.boxShadow};
+  background-image: ${(props) => props.backgroundImage};
+  padding-left: 0.75em;
+  :focus {
+    outline: none !important;
+    outline-offset: none !important;
+  }
+`;
+const Option = styled.option`
+  font-family: ${(props) => props.fontFamily};
+  font-size: 0.875em;
+  font-weight: 400;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: ${(props) => props.color};
+  width: auto;
+`;
+
+export default function CountrySelector(props) {
+  const {
+    onChange,
+    setUserData,
+    id,
+    height,
+    width,
+    border,
+    backgroundColor,
+    borderRadius,
+    boxSizing,
+    boxShadow,
+    backgroundImage,
+    backgroundSize,
+    color,
+    fontSize,
+    fontFamily,
+    fontWeight,
+  } = props;
+  let array = ["Egypt", "UK", "Canda"];
+  return (
+    <Container>
+      <Select
+        as="select"
+        id={id}
+        onChange={(e) => onChange(e, setUserData)}
+        fontFamily={fontFamily}
+        boxSizing={boxSizing}
+        height={height}
+        width={width}
+        border={border}
+        borderRadius={borderRadius}
+        backgroundColor={backgroundColor}
+        backgroundImage={backgroundImage}
+        backgroundSize={backgroundSize}
+        fontSize={fontSize}
+        fontWeight={fontWeight}
+        color={color}
+        box-shadow={boxShadow}
+      >
+        {array.map((item, index) => (
+          <Option
+            key={index}
+            value={item}
+            fontFamily={fontFamily}
+            color={color}
+          >
+            {item}
+          </Option>
+        ))}
+      </Select>
+    </Container>
+  );
+}
+CountrySelector.propTypes = {
+  height: PropTypes.string,
+  width: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  borderRadius: PropTypes.string,
+  boxSizing: PropTypes.string,
+  boxShadow: PropTypes.string,
+  backgroundImage: PropTypes.string,
+  backgroundSize: PropTypes.string,
+  fontFamily: PropTypes.string,
+  fontSize: PropTypes.string,
+  fontWeight: PropTypes.string,
+  color: PropTypes.string,
+  array: PropTypes.array,
+  onChange: PropTypes.func,
+  setUserData: PropTypes.func,
+  id: PropTypes.string,
+};
+
+CountrySelector.defaultProps = {
+  height: "2.2em",
+  width: "100%",
+  backgroundColor: "#ffffff",
+  borderRadius: "3px",
+  boxSizing: "border-box",
+  boxShadow: "none",
+  backgroundImage: `url(${arrow})`,
+  backgroundSize: "1.712em auto, 100%",
+  fontSize: "0.875em",
+  color: "#081c64",
+  fontWeight: 600,
+};
---
+++ src/components/PhoneField/index.js
@@ -0,0 +1,73 @@
+import React, { Fragment } from "react";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
+import { isValidPhoneNumber } from "react-phone-number-input/max";
+import styled from "styled-components";
+import propTypes from "prop-types";
+
+const Phone = styled(PhoneInput)`
+  .PhoneInputInput {
+    width: 100%;
+    height: 2.2em;
+    border-radius: 3px;
+    border: solid 1px #081c64;
+    background-color: #ffffff;
+    padding-left: 0.75em;
+    ::placeholder {
+      font-family: SFProText;
+      font-size: 0.875em;
+      font-weight: 300;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: normal;
+      letter-spacing: normal;
+      color: #7680a6;
+    }
+    :focus {
+      outline: none !important;
+      outline-offset: none !important;
+    }
+  }
+  .PhoneInputCountryIcon {
+    height: 2.2em;
+    width: 2.2em;
+    border-radius: 3px;
+    border: solid 1px #081c64;
+  }
+  --PhoneInputCountrySelectArrow-color: #0d9909;
+  .PhoneInputCountryIconImg {
+    width: 100%;
+    height: 100%;
+    display: block;
+    object-fit: cover;
+  }
+`;
+export default function PhoneField(props) {
+  const { phone, setUserData } = props;
+  return (
+    <Fragment>
+      <Phone
+        defaultCountry="SA"
+        placeholder="Enter phone number"
+        value={phone}
+        onChange={(e) => setUserData(e)}
+        error={
+          phone
+            ? isValidPhoneNumber(phone)
+              ? undefined
+              : "Invalid phone number"
+            : "Phone number required"
+        }
+      />
+      {phone && !isValidPhoneNumber(phone) && (
+        <p style={{ color: "red" }}>The phone number is invalid</p>
+      )}
+    </Fragment>
+  );
+}
+
+PhoneField.propTypes = {
+  phone: propTypes.string,
+  setUserData: propTypes.func,
+  onChange: propTypes.func,
+};
