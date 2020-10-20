264c4e1fb10fbd187272ea8e5a31fe7ad657fae9
Marwa92 <engmarwayehya@yahoo.com>
2020-10-20 09:15:28 +0200

    create steps component with styling

--- src/containers/Auth/Signup/Message.js
+++ src/containers/Auth/Signup/Message.js
@@ -1,5 +1,45 @@
 import React from "react";
+import styled from "styled-components";
+import { Button } from "react-bootstrap";
+import { signUp } from "../../../services/Services";
 
-export default function Message() {
-  return <div>You're all set.Ready?</div>;
+const Wrapper = styled.div`
+  margin-top: 2.5em;
+  margin-left: 20%;
+  margin-right: 20%;
+`;
+
+const MessageContainer = styled.div`
+  margin-top: 1em;
+  background-color: #fff;
+  padding-top: 1em;
+  padding-left: 0.7em;
+  padding-right: 0.7em;
+  padding-bottom: 0.7em;
+  border-radius: 2px;
+  color: red;
+`;
+
+const Email = styled.p`
+  color: black;
+`;
+
+const ConfirmButton = styled(Button)`
+  margin-top: 20px;
+`;
+export default function Message(props) {
+  const { userData } = props;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signUp(userData);
+  };
+  return (
+    <Wrapper>
+      You're all set.Ready?
+      <MessageContainer>
+        We will send message to this email <Email>example@example.com</Email>
+        <ConfirmButton onClick={handleSubmit}>Confirm</ConfirmButton>
+      </MessageContainer>
+    </Wrapper>
+  );
 }
