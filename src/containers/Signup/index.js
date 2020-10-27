65961b01bda216f0e0163b2f9539a72498587060
Marwa92 <engmarwayehya@yahoo.com>
2020-10-28 01:42:22 +0200

    WIP: new version

---
+++ src/containers/Signup/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import Bar from "../../components/Bar";
+
+const Container = styled.div``;
+
+export default function Signup() {
+  return (
+    <Container>
+      <Bar />
+      {/* <Steps /> */}
+    </Container>
+  );
+}
